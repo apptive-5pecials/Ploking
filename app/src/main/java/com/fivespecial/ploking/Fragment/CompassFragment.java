44499cfaa40f6abf916990e6708129108b4bd540
A-jeong <ljmpretty98@gmail.com>
2019-08-27 14:22:12 +0900

    add compass

---
+++ app/src/main/java/com/fivespecial/ploking/Fragment/CompassFragment.java
@@ -0,0 +1,125 @@
+package com.fivespecial.ploking.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.fivespecial.ploking.AdapterEtc.Compass;
+import com.fivespecial.ploking.AdapterEtc.SOTWFormatter;
+import com.fivespecial.ploking.R;
+
+public class CompassFragment extends Fragment {
+
+    private static final String TAG = "CompassActivity";
+
+    private Compass compass;
+    private ImageView arrowView;
+    private TextView sotwLabel;  // SOTW is for "side of the world"
+
+    private float currentAzimuth;
+    private SOTWFormatter sotwFormatter;
+
+    public CompassFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view;
+        view = inflater.inflate(R.layout.compass, null);
+
+        sotwFormatter = new SOTWFormatter(getActivity());
+
+        arrowView = view.findViewById(R.id.hands);
+        sotwLabel = view.findViewById(R.id.sotw_label);
+        setupCompass();
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "start compass");
+        compass.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        compass.stop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        compass.start();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "stop compass");
+        compass.stop();
+    }
+
+    private void setupCompass() {
+        compass = new Compass(getActivity());
+        Compass.CompassListener cl = getCompassListener();
+        compass.setListener(cl);
+    }
+
+    private void adjustArrow(float azimuth) {
+        Log.d(TAG, "will set rotation from " + currentAzimuth + " to "
+                + azimuth);
+
+        Animation an = new RotateAnimation(-currentAzimuth, -azimuth,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        currentAzimuth = azimuth;
+
+        an.setDuration(500);
+        an.setRepeatCount(0);
+        an.setFillAfter(true);
+
+        arrowView.startAnimation(an);
+    }
+
+    private void adjustSotwLabel(float azimuth) {
+        sotwLabel.setText(sotwFormatter.format(azimuth));
+    }
+
+    private Compass.CompassListener getCompassListener() {
+        return new Compass.CompassListener() {
+            @Override
+            public void onNewAzimuth(final float azimuth) {
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adjustArrow(azimuth);
+                        adjustSotwLabel(azimuth);
+                    }
+                });
+            }
+        };
+    }
+
+}
