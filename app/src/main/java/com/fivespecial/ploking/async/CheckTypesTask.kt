04ef7fddbc95864a75bf59e7ff4bc9dee5145968
leeohhyoung <lee2928030@naver.com>
2019-10-09 23:08:20 +0900

    AsyncTask declared inner class may be occur memoryLeak. Separate into another class file

---
+++ app/src/main/java/com/fivespecial/ploking/async/CheckTypesTask.kt
@@ -0,0 +1,40 @@
+/*
+ * Created by Lee Oh Hyoung on 2019-10-09 .. 
+ */
+package com.fivespecial.ploking.async
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.os.AsyncTask
+
+class CheckTypesTask(activity: Activity) : AsyncTask<Void, Void, Void>() {
+
+    private var asyncDialog = ProgressDialog(activity)
+
+    override fun onPreExecute() {
+        asyncDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+        asyncDialog.setMessage("로딩중입니다..")
+
+        // show dialog
+        asyncDialog.show()
+        super.onPreExecute()
+    }
+
+    override fun doInBackground(vararg args: Void): Void? {
+        try {
+            for (i in 0..4) {
+                //asyncDialog.setProgress(i * 30);
+                Thread.sleep(500)
+            }
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        return null
+    }
+
+    override fun onPostExecute(result: Void) {
+        asyncDialog.dismiss()
+        super.onPostExecute(result)
+    }
+}
