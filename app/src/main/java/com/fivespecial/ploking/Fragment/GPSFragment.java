8eafbf8db20611413c75104f047ec0000cdb63b9
leeohhyoung <lee2928030@naver.com>
2019-10-09 17:31:13 +0900

    GpsFragment 코드 정리. 로직을 수정한 부분은 없음.

--- app/src/main/java/com/fivespecial/ploking/Fragment/GPSFragment.java
+++ app/src/main/java/com/fivespecial/ploking/Fragment/GPSFragment.java
@@ -25,16 +25,17 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
+import com.fivespecial.ploking.R;
 import com.fivespecial.ploking.maps.BinLocation;
 import com.fivespecial.ploking.maps.Calculation;
 import com.fivespecial.ploking.maps.DataAdapter;
-import com.fivespecial.ploking.R;
 import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.NaverMapSdk;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.InfoWindow;
@@ -49,9 +50,13 @@ import static android.content.Context.LOCATION_SERVICE;
 
 public class GPSFragment extends Fragment {
 
-    //TAG
     private static final String TAG = "GPSFragment";
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+
+    private static final double DEFAULT_CAMERA_LATITUDE = 35.232286;
+    private static final double DEFAULT_CAMERA_LONGITUDE = 129.085166;
+    private static final double DEFAULT_BIN_LATITUDE = 35.232286;
+    private static final double DEFAULT_BIN_LONGITUDE = 129.085166;
 
     //custom-info-window
     private static class InfoWindowAdapter extends InfoWindow.DefaultTextAdapter {
@@ -74,7 +79,6 @@ public class GPSFragment extends Fragment {
     //garbage bins
     private Calculation calculation;
     private List<BinLocation> binLocationList;
-    private BinLocation binLocation;
     private TextView txtNearBin;
     private int binCount;
 
@@ -96,18 +100,9 @@ public class GPSFragment extends Fragment {
     private Boolean tButtonclicked = false;
     private Thread timeThread;
 
-
-    //kcal
-    private int weight = 70;
-    private float coef = 0.001f; // 1초당 운동계수
     private float calorie = 0;
 
     private FusedLocationSource locationSource;
-    private MapFragment mapFragment;
-
-    public GPSFragment() {
-
-    }
 
     public static GPSFragment newInstance(){
         return new GPSFragment();
@@ -119,10 +114,14 @@ public class GPSFragment extends Fragment {
 
         binCount = 0;
 
-        //API를 호출해 클라이언트 ID를 지정
-        NaverMapSdk.getInstance(getActivity()).setClient(
-                new NaverMapSdk.NaverCloudPlatformClient("0fwsvimx0a"));
+        // onMapReady 에 있던것을 onCreate 에서 하는것으로 변경
+        initLoadDB();
 
+        //API를 호출해 클라이언트 ID를 지정
+        if(getActivity() != null) {
+            NaverMapSdk.getInstance(getActivity()).setClient(
+                    new NaverMapSdk.NaverCloudPlatformClient("0fwsvimx0a"));
+        }
     }
 
     @Nullable
@@ -148,21 +147,28 @@ public class GPSFragment extends Fragment {
         txtTime = view.findViewById(R.id.tv_fragment_four_time);
         txtKcal = view.findViewById(R.id.tv_fragment_four_calorie);
 
-        // Naver MapFragment 초기화
-        FragmentManager fm = getFragmentManager();
-        mapFragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-
-        if(mapFragment == null){
-            mapFragment = MapFragment.newInstance();
-            fm.beginTransaction().add(R.id.fragmentBorc, mapFragment).commit();
-        }
-
         locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
-        /**
-         * @desciption : 비동기로 NaverMap 객체를 가져옴. NaverMap 객체가 준비되면 callback 의 onMapReady(NaverMap) 호출됨.
-         */
-        mapFragment.getMapAsync(this::onMapReady);
+        try {
+
+            // Naver MapFragment 초기화
+            MapFragment mapFragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+            // 초기 위치 설정
+            NaverMapOptions options = new NaverMapOptions()
+                    .camera(new CameraPosition(new LatLng(35.232286, 129.085166), 14));
+
+            mapFragment = MapFragment.newInstance(options);
+            getFragmentManager().beginTransaction().add(R.id.fragmentBorc, mapFragment).commit();
+
+            //비동기로 NaverMap 객체를 가져옴. NaverMap 객체가 준비되면 callback 의 onMapReady(NaverMap) 호출됨.
+            assert mapFragment != null;
+            mapFragment.getMapAsync(this::onMapReady);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, e + "에러 발생");
+        }
 
         // 시작버튼 클릭 리스너
         startButton.setOnClickListener((View v) -> {
@@ -188,7 +194,6 @@ public class GPSFragment extends Fragment {
             informLayout.setVisibility(View.GONE);
 
             SharedPreferences sFile = getActivity().getSharedPreferences("sFile", Context.MODE_PRIVATE);
-
             SharedPreferences.Editor editor = sFile.edit();
 
             float updateDistance = sFile.getFloat("distance", 0);
@@ -237,7 +242,7 @@ public class GPSFragment extends Fragment {
             Location loc = null;
 
             if (lm != null) {
-                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, Loclist);
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, setLocationListener());
                 loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             }
 
@@ -261,8 +266,7 @@ public class GPSFragment extends Fragment {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,  @NonNull int[] grantResults) {
 
         if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
             return;
@@ -278,47 +282,60 @@ public class GPSFragment extends Fragment {
         double dLat, dLong;
         final InfoWindow binsInfo = new InfoWindow();
 
-
-
         final LocationManager lm = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
         Location location;
 
+        try {
+            //position 속성을 지정하면 위치 오버레이의 좌표를 변경할 수 있다.
+            //처음 생성된 위치 오버레이는 카메라의 초기 좌표에 위치해 있다.
+            location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+        catch(SecurityException | NullPointerException e){
+            Log.v(TAG, "CANNOT be find GPS signal.");
+            latitude = DEFAULT_CAMERA_LATITUDE;
+            longitude = DEFAULT_CAMERA_LONGITUDE;
+        }
+
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(latitude, longitude));
+
         // uisetting
         UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);
 
         naverMap.setLocationSource(locationSource);
-
-        LocationOverlay locationOverlay = naverMap.getLocationOverlay();
-        locationOverlay.setVisible(true);
-
-        //marker
-        initLoadDB();
-
         naverMap.setOnMapClickListener((coord, point) -> {
             binsInfo.close();
         });
-        
+        naverMap.getLocationOverlay().setVisible(true);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
 
-        for(int i = 0; i < binLocationList.size(); i++){
-            binLocation = binLocationList.get(i);
+        // 오버레이 : 지리적 정보를 시작적으로 나타내느 요소. 개발자가 지도 위에 자유롭게 배치할 수 있음.
+        // 마커, 정보창(InfoWindow), 셰이프 등
+        LocationOverlay locationOverlay = naverMap.getLocationOverlay();
+        locationOverlay.setVisible(true);
+        locationOverlay.setBearing(90); // 방위를 지정함.
+        locationOverlay.setIconWidth(100); // 아이콘의 너비를 지정함
+        locationOverlay.setIconHeight(100); // 아이콘의 높이를 지정함.
+        locationOverlay.setPosition(new LatLng(latitude, longitude)); // 위치 지정
 
+        for(BinLocation binLocation : binLocationList){
 
             if(binLocation.getLatitude() != null && binLocation.getLongitude() != null) {
                 dLat = binLocation.getLatitude();
                 dLong = binLocation.getLongitude();
             } else {
-                dLat = 0.0;
-                dLong = 0.0;
+                dLat = DEFAULT_BIN_LATITUDE;
+                dLong = DEFAULT_BIN_LONGITUDE;
             }
 
-//            final InfoWindow binsInfo = new InfoWindow();
-//            binsInfo.setPosition(new LatLng(dLat, dLong));
             binsInfo.setAdapter(new InfoWindowAdapter(getActivity()));
             binsInfo.setOnClickListener(overlay -> {
                 binsInfo.close();
                 return true;
             });
-
 
             Marker bins = new Marker();
             bins.setPosition(new LatLng(dLat, dLong));
@@ -331,128 +348,94 @@ public class GPSFragment extends Fragment {
             bins.setMap(naverMap);
         }
 
-
-
-        try {
-            location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
-            //position 속성을 지정하면 위치 오버레이의 좌표를 변경할 수 있다.
-            //처음 생성된 위치 오버레이는 카메라의 초기 좌표에 위치해 있다.
-        }
-        catch(SecurityException | NullPointerException e){
-            Log.v(TAG, "CANNOT be find GPS signal.");
-            longitude = 129.04378;
-            latitude = 35.237396;
-        }
         binCount = calculation.nearBins(binLocationList, latitude, longitude);
         txtNearBin.setText(getString(R.string.near_bin_notice, binCount));
-        locationOverlay.setPosition(new LatLng(latitude, longitude));
-
-        Marker marker = new Marker();
-        marker.setPosition(new LatLng(latitude, longitude));
-
-
-        //bearing 속성을 지정하면 위치 오버레이의 베어링을 변경할 수 있다.
-        // flat이 true인 마커의 angle과 유사하게 아이콘이 지도를 기준으로 회전한다.
-        locationOverlay.setBearing(90);
-
-        locationOverlay.setIconWidth(100);
-        locationOverlay.setIconHeight(100);
-
-        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-
-        //uiSettings...
-        uiSettings.setLocationButtonEnabled(true);
 
     }
 
-    LocationListener Loclist = new LocationListener(){
 
-        @Override
-        public void onLocationChanged(Location location) {
+    private LocationListener setLocationListener() {
 
-            int binCount = 0;
+        return new LocationListener(){
 
-            try{
-                //start location manager
-                LocationManager lm =(LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+            @Override
+            public void onLocationChanged(Location location) {
 
-                //Request new location
-                lm.requestLocationUpdates(lm.GPS_PROVIDER, 0,0, Loclist);
+                try{
+                    //start location manager
+                    LocationManager lm =(LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
 
-                //Get new location
-                Location loc = lm.getLastKnownLocation(lm.GPS_PROVIDER);
+                    //Request new location
+                    lm.requestLocationUpdates(lm.GPS_PROVIDER, 0,0, this);
 
-                //get the current lat and long
-                currentLat = loc.getLatitude();
-                currentLon = loc.getLongitude();
+                    //Get new location
+                    Location loc = lm.getLastKnownLocation(lm.GPS_PROVIDER);
 
-                binCount = calculation.nearBins(binLocationList, currentLat, currentLon);
+                    //get the current lat and long
+                    currentLat = loc.getLatitude();
+                    currentLon = loc.getLongitude();
 
-                txtNearBin.setText(getString(R.string.near_bin_notice, binCount));
+                    binCount = calculation.nearBins(binLocationList, currentLat, currentLon);
+
+                    txtNearBin.setText(getString(R.string.near_bin_notice, binCount));
 
 
-                if(isRunning){
-                    if(tButtonclicked){
+                    if(isRunning){
+                        if(tButtonclicked){
+                            lastLat = currentLat;
+                            lastLon = currentLon;
+                            tButtonclicked = false;
+                        }
+
+                        Location locationA = new Location("point A");
+                        locationA.setLatitude(lastLat);
+                        locationA.setLongitude(lastLon);
+
+                        Location locationB = new Location("point B");
+                        locationB.setLatitude(currentLat);
+                        locationB.setLongitude(currentLon);
+
+                        float distanceMeters = locationA.distanceTo(locationB);
+                        Log.v(TAG, String.format("%f", lastLon));
+                        Log.v(TAG, String.format("%f", currentLon));
+
+                        distance_sum += distanceMeters;
+
                         lastLat = currentLat;
                         lastLon = currentLon;
-                        tButtonclicked = false;
+
+                        txtDistance.setText(String.format("%.1f m",distance_sum ));
                     }
-
-                    Location locationA = new Location("point A");
-                    locationA.setLatitude(lastLat);
-                    locationA.setLongitude(lastLon);
-
-                    Location locationB = new Location("point B");
-                    locationB.setLatitude(currentLat);
-                    locationB.setLongitude(currentLon);
-
-                    float distanceMeters = locationA.distanceTo(locationB);
-                    Log.v(TAG, String.format("%f", lastLon));
-                    Log.v(TAG, String.format("%f", currentLon));
-
-                    distance_sum += distanceMeters;
-
-                    lastLat = currentLat;
-                    lastLon = currentLon;
-
-                    txtDistance.setText(String.format("%.1f m",distance_sum ));
+                }
+                catch(SecurityException | NullPointerException e){
+                    e.printStackTrace();
                 }
             }
-            catch(SecurityException | NullPointerException e){
-                e.printStackTrace();
-            }
-        }
 
-        @Override
-        public void onProviderDisabled(String provider) {
-        }
+            @Override
+            public void onProviderDisabled(String provider) { }
 
+            @Override
+            public void onProviderEnabled(String provider) { }
 
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) { }
 
-        @Override
-        public void onProviderEnabled(String provider) {
-        }
-
-
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-        }
-
-    };
+        };
+    }
 
     @SuppressLint("HandlerLeak")
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
+
+            //1000이 1초 1000*60 은 1분 1000*60*10은 10분 1000*60*60은 한시간
             int sec_sum = msg.arg1;
             int sec = msg.arg1 % 60;
             int min = msg.arg1 / 60;
-            //1000이 1초 1000*60 은 1분 1000*60*10은 10분 1000*60*60은 한시간
 
-            //소모 칼로리
+            int weight = 70; //소모 칼로리(kcal)
+            float coef = 0.001f; // 1초당 운동계수
             calorie = weight * coef * sec_sum;
 
             @SuppressLint("DefaultLocale") String result = String.format("%02d:%02d", min, sec);
