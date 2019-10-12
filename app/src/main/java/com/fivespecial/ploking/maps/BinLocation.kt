a8617da783617e4f37928af08306572f87821bb6
leeohhyoung <lee2928030@naver.com>
2019-10-12 20:32:33 +0900

    Change name DefaultBinLocation to BinLocationConstant

--- app/src/main/java/com/fivespecial/ploking/maps/BinLocation.kt
+++ app/src/main/java/com/fivespecial/ploking/maps/BinLocation.kt
@@ -4,8 +4,8 @@ import android.location.Location
 import java.io.Serializable
 
 data class BinLocation(var id: String? = null,
-                       var latitude: Double = DefaultBinLocation.DEFAULT_BIN_LATITUDE,
-                       var longitude: Double = DefaultBinLocation.DEFAULT_BIN_LONGITUDE,
+                       var latitude: Double = BinLocationConstant.DEFAULT_BIN_LATITUDE,
+                       var longitude: Double = BinLocationConstant.DEFAULT_BIN_LONGITUDE,
                        var description: String = ""): Serializable {
 
     fun toLocation(): Location {
--- app/src/main/java/com/fivespecial/ploking/maps/BinLocationConstant.kt
+++ app/src/main/java/com/fivespecial/ploking/maps/BinLocationConstant.kt
@@ -3,7 +3,7 @@ package com.fivespecial.ploking.maps
 /*
  * Created by Lee Oh Hyoung on 2019-10-12..
  */
-object DefaultBinLocation {
+object BinLocationConstant {
 
     const val DEFAULT_BIN_LATITUDE = 35.232286
     const val DEFAULT_BIN_LONGITUDE = 129.085166
