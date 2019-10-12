67323b9f2fe9e9060d5e7ed92bf5f9d05950cd02
leeohhyoung <lee2928030@naver.com>
2019-10-12 19:53:27 +0900

    Calculate nearest bin, show its location in HomeFragment.

--- app/src/main/java/com/fivespecial/ploking/fragment/HomeFragment.java
+++ app/src/main/java/com/fivespecial/ploking/fragment/HomeFragment.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Looper;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,6 +16,8 @@ import com.fivespecial.ploking.R;
 import com.fivespecial.ploking.activity.TrashActivity;
 import com.fivespecial.ploking.async.CheckTypesTask;
 import com.fivespecial.ploking.base.BaseFragment;
+import com.fivespecial.ploking.maps.BinLocation;
+import com.fivespecial.ploking.maps.DataAdapter;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -21,6 +25,10 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class HomeFragment extends BaseFragment {
 
@@ -31,6 +39,12 @@ public class HomeFragment extends BaseFragment {
     private TextView tvDistance;
     private TextView tvKcal;
     private ImageView trashButton;
+
+    private TextView nearestBinDescription;
+    private TextView nearestBinDistance;
+
+    private List<BinLocation> mBinLocationList;
+    private DataAdapter mHomeDbHelper;
 
     private FusedLocationProviderClient mFusedLocationClient;
     private LocationCallback mLocationCallback;
@@ -43,9 +57,14 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     public void initComponent(@NotNull View view) {
+        mHomeDbHelper = new DataAdapter(getActivity());
+
         tvDistance = view.findViewById(R.id.home_steps);
         tvKcal = view.findViewById(R.id.home_kcals);
         trashButton = view.findViewById(R.id.button_trash);
+
+        nearestBinDescription = view.findViewById(R.id.fragment_home_nearest_bin_description_text_view);
+        nearestBinDistance = view.findViewById(R.id.fragment_home_nearest_bin_distance_text_view);
     }
 
     @Override
@@ -67,6 +86,8 @@ public class HomeFragment extends BaseFragment {
     public void setupImplementation() {
 
         if (getActivity() != null) {
+
+            mBinLocationList = mHomeDbHelper.initLoadData();
 
             setupFusedLocationClient();
             setLocationRequest();
@@ -135,16 +156,49 @@ public class HomeFragment extends BaseFragment {
             @Override
             public void onLocationResult(LocationResult locationResult) {
 
-                if(locationResult != null) {
+                if(locationResult != null && locationResult.getLocations().size() == 1) {
 
-                    for(Location location : locationResult.getLocations()) {
-                        Log.d("LocationResult", "LocationResult.location size : " + locationResult.getLocations().size());
-                        Log.d("LocationResult", "위도 : " + location.getLatitude() + ", 경도 : " + location.getLongitude());
+                    // 가장 근접한 쓰레기통을 구하고, 두 지점 사이의 거리를 구함.
+                    Location myLocation = locationResult.getLocations().get(0);
+                    ArrayList<Pair<BinLocation, Double>> tempBinList = new ArrayList<>();
+
+                    for(BinLocation bin : mBinLocationList) {
+
+                        double distanceTo = myLocation.distanceTo(bin.toLocation());
+
+                        tempBinList.add(new Pair<>(bin, distanceTo));
                     }
+
+                    Pair<BinLocation, Double> nearestBin = tempBinList.get(0); // 임의로 가장 가까운 쓰레기통을 tempList[0] 으로 설정
+
+                    for(Pair<BinLocation, Double> pair : tempBinList) {
+                        if(nearestBin.second.compareTo(pair.second) >= 0) {
+                            nearestBin = pair;
+                        }
+                    }
+
+                    showNearestBinLocation(nearestBin);
 
                 }
             }
         };
+    }
 
+    private void showNearestBinLocation(Pair<BinLocation, Double> nearBinLocation) {
+
+        if(nearestBinDescription != null && nearestBinDistance != null) {
+
+            nearestBinDescription.setText(nearBinLocation.first.getDescription());
+
+            int integerDistance = nearBinLocation.second.intValue();
+
+            if(integerDistance >= 1000) { // 거리가 1000m 이상이라면 KM로 나타냄.
+                nearestBinDistance.setText(getString(R.string.fragment_home_nearest_bin_distance_format_kilo_meter, (integerDistance / 1000)));
+
+            } else { // 거리가 1000m 이하라면 Meter 로 나타냄.
+                nearestBinDistance.setText(getString(R.string.fragment_home_nearest_bin_distance_format_meter, integerDistance));
+            }
+
+        }
     }
 }
