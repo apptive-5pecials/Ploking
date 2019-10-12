ea886c0a2dd6a0864aea989dc13d1aab99034e09
leeohhyoung <lee2928030@naver.com>
2019-10-12 18:39:27 +0900

    Change permissionCheck Condition, useless code in MainActivity.java

--- app/src/main/java/com/fivespecial/ploking/activity/MainActivity.java
+++ app/src/main/java/com/fivespecial/ploking/activity/MainActivity.java
@@ -11,14 +11,19 @@ import android.os.Handler;
 
 import com.fivespecial.ploking.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final int LOCATION_PERMISSION_REQUEST_CODE= 1000;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 권한 요청이 필요한 Permission 을 String Array 에 추가
+        String[] requiredPermissions = new String[]{ Manifest.permission.ACCESS_FINE_LOCATION };
 
         //권한 요청
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
@@ -26,30 +31,20 @@ public class MainActivity extends AppCompatActivity {
         //권한 설정이 되어 있는지 확인
         if(permissionCheck != PackageManager.PERMISSION_GRANTED){
 
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){}
-            else{
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}
-                        , LOCATION_PERMISSION_REQUEST_CODE);
+            if(!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // 사용자가 권한 요청을 거부하지 않았다면, 이 분기를 타게 됨.
+                ActivityCompat.requestPermissions(this, requiredPermissions, LOCATION_PERMISSION_REQUEST_CODE);
             }
         }
 
-/*
-        Intent intent = new Intent(this, TabbedActivity.class);
-        startActivity(intent);
-        finish();
-
-
-
-        /*
-        */
         Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent1 = new Intent(getBaseContext(),TabbedActivity.class);
-                startActivity(intent1);
-                finish();
-            }
+        handler.postDelayed(() -> {
+
+            // 설정한 delay 후에 TabbedActivity 실행
+            startActivity(new Intent(getBaseContext(),TabbedActivity.class));
+
+            finish();
         }, 500);
 
 
